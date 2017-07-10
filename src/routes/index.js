fb9a284ceb4c984ef6048024fb82a5dc93761f16
chrisdennett <chrisdennett@gmail.com>
2017-07-10 11:19:14 +0100

    Remove unnecessary divs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,29 +9,29 @@ import UserControls from "../components/User/UserControls";
 import MyGalleries from "../components/MyGalleries/MyGalleries";
 import ArtworkEditor from "../components/ArtworkEditor/ArtworkEditor";
 
+/*
+The two switch statements are the simplest way I've found to ensure parameters are passed to the UserControls
+*/
+
 export default (
     <Provider store={store}>
         <BrowserRouter>
             <div>
-                <div>
-                    <Switch>
-                        <Route path="/gallery/:galleryId/artwork/:artworkId" component={UserControls}/>
-                        <Route path="/gallery/:galleryId" component={UserControls}/>
-                        <Route path="/myGalleries" component={UserControls}/>
-                        <Route path="/artwork-editor/:artworkId" component={UserControls}/>
-                        <Route path="/" component={UserControls}/>
-                    </Switch>
-                </div>
-                <div>
-                    {/*<UserControls />*/}
-                    <Switch>
-                        <Route path="/gallery/:galleryId/artwork/:artworkId" component={Gallery}/>
-                        <Route path="/gallery/:galleryId" component={Gallery}/>
-                        <Route path="/myGalleries" component={MyGalleries}/>
-                        <Route path="/artwork-editor/:artworkId" component={ArtworkEditor}/>
-                        <Route path="/" component={Home}/>
-                    </Switch>
-                </div>
+                <Switch>
+                    <Route path="/gallery/:galleryId/artwork/:artworkId" component={UserControls}/>
+                    <Route path="/gallery/:galleryId" component={UserControls}/>
+                    <Route path="/myGalleries" component={UserControls}/>
+                    <Route path="/artwork-editor/:artworkId" component={UserControls}/>
+                    <Route path="/" component={UserControls}/>
+                </Switch>
+
+                <Switch>
+                    <Route path="/gallery/:galleryId/artwork/:artworkId" component={Gallery}/>
+                    <Route path="/gallery/:galleryId" component={Gallery}/>
+                    <Route path="/myGalleries" component={MyGalleries}/>
+                    <Route path="/artwork-editor/:artworkId" component={ArtworkEditor}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
             </div>
         </BrowserRouter>
     </Provider>
