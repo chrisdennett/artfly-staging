f4b4a013218da7aa730dd808fc94f9e311421d23
chrisdennett <chrisdennett@gmail.com>
2017-07-12 14:32:14 +0100

    Refactored routes so navigation routes and app routes are working off the same code.

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,36 +6,45 @@ import store from '../store';
 import ArtistGalleryContainer from '../components/ArtistGallery/ArtistGalleryContainer';
 import SettingsContainer from "../components/Settings/SettingsContainer";
 import AddOrEditArtistContainer from "../components/AddOrEditArtist/AddOrEditArtistContainer";
-import Home from '../components/Home/Home';
-// import Gallery from '../components/ArtistGallery/Gallery';
 import ArtworkContainer from '../components/Artwork/ArtworkContainer';
-
+import Home from '../components/Home/Home';
 import UserControls from "../components/User/UserControls";
 import ArtworkEditor from "../components/ArtworkEditor/ArtworkEditor";
 
+const routes = [
+    { path: "/gallery/:galleryId/artwork/:artworkId", component: ArtworkContainer },
+    { path: "/gallery/:galleryId", component: ArtistGalleryContainer },
+    { path: "/settings", component: SettingsContainer },
+    { path: "/artwork-editor/:artworkId", component: ArtworkEditor },
+    { path: "/add-or-edit-artist/:artistId?", component: AddOrEditArtistContainer },
+    { path: "/", component: Home }
+];
+
 /*
-~ The two switch statements are the simplest way I've found to ensure parameters are passed to the UserControls
-*/
+ ~ The two switch statements are the simplest way I've found to ensure parameters are passed to the UserControls
+ */
 export default (
     <Provider store={store}>
         <BrowserRouter>
             <div>
                 <Switch>
-                    <Route path="/gallery/:galleryId/artwork/:artworkId" component={UserControls}/>
-                    <Route path="/gallery/:galleryId" component={UserControls}/>
-                    <Route path="/settings" component={UserControls}/>
-                    <Route path="/artwork-editor/:artworkId" component={UserControls}/>
-                    <Route path="/add-or-edit-artist/:artistId?" component={UserControls}/>
-                    <Route path="/" component={UserControls}/>
+                    {routes.map((route, index) => (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            component={UserControls}
+                        />
+                    ))}
                 </Switch>
 
                 <Switch>
-                    <Route path="/gallery/:galleryId/artwork/:artworkId" component={ArtworkContainer}/>
-                    <Route path="/gallery/:galleryId" component={ArtistGalleryContainer}/>
-                    <Route path="/settings" component={SettingsContainer}/>
-                    <Route path="/artwork-editor/:artworkId" component={ArtworkEditor}/>
-                    <Route path="/add-or-edit-artist/:artistId?" component={AddOrEditArtistContainer}/>
-                    <Route path="/" component={Home}/>
+                    {routes.map((route, index) => (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            component={route.component}
+                        />
+                    ))}
                 </Switch>
             </div>
         </BrowserRouter>
