4a97fbd604ddaf8a09e0014e76461b9d4a37ed24
chrisdennett <chrisdennett@gmail.com>
2017-07-18 15:52:54 +0100

    Updated google cloud function code with code from firebase examples github page.

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,65 @@
 const functions = require('firebase-functions');
+const mkdirp = require('mkdirp-promise');
 const gcs = require('@google-cloud/storage')();
-const spawn = require('child-process-promise').spawn;
+const exec = require('child-process-promise').exec;
+const LOCAL_TMP_FOLDER = '/tmp/';
 
-/*const admin = require('firebase-admin');
- admin.initializeApp(functions.config().firebase);*/
+// Max height and width of the thumbnail in pixels.
+const THUMB_MAX_HEIGHT = 200;
+const THUMB_MAX_WIDTH = 200;
+// Thumbnail prefix added to file names.
+const THUMB_PREFIX = 'thumb_';
 
-exports.generateThumbnail = functions.storage.object()
-    .onChange(event => {
-        const object = event.data;
-        const filePath = object.name;
-        const fileName = filePath.split('/').pop();
-        const fileBucket = object.bucket;
-        const bucket = gcs.bucket(fileBucket);
-        const tempFilePath = `/tmp/${fileName}`;
+/**
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
+ * ImageMagick.
+ */
+exports.generateThumbnail = functions.storage.object().onChange(event => {
+    const filePath = event.data.name;
+    const filePathSplit = filePath.split('/');
+    const fileName = filePathSplit.pop();
+    const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
+    const thumbFilePath = `${fileDir}${THUMB_PREFIX}${fileName}`;
+    const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
+    const tempLocalFile = `${tempLocalDir}${fileName}`;
+    const tempLocalThumbFile = `${LOCAL_TMP_FOLDER}${thumbFilePath}`;
 
-        if(fileName.startsWith('thumb_')){
-            console.log("Already a Thumbnail");
-            return;
-        }
+    // Exit if this is triggered on a file that is not an image.
+    if (!event.data.contentType.startsWith('image/')) {
+        console.log('This is not an image.');
+        return;
+    }
 
-        if (!object.contentType.startsWith('image/')) {
-            console.log("This is not an image.");
-            return;
-        }
+    // Exit if the image is already a thumbnail.
+    if (fileName.startsWith(THUMB_PREFIX)) {
+        console.log('Already a Thumbnail.');
+        return;
+    }
 
-        if (object.resourceState === 'not_exists') {
-            console.log("This is a deletion event.");
-            return;
-        }
+    // Exit if this is a move or deletion event.
+    if (event.data.resourceState === 'not_exists') {
+        console.log('This is a deletion event.');
+        return;
+    }
 
+    // Create the temp directory where the storage file will be downloaded.
+    return mkdirp(tempLocalDir).then(() => {
+        // Download file from bucket.
+        const bucket = gcs.bucket(event.data.bucket);
         return bucket.file(filePath).download({
-            destination: tempFilePath
-        })
-            .then(() => {
-                console.log("Image downloaded locally to tempFilePath: ", tempFilePath);
-                return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
-            })
-            .then(() => {
-                console.log('Thumbnail created');
-                const thumbFilePath = filePath.replace(/(\/)?([^\/]*)$/, '$1thumb_$2');
-
-                return bucket.upload(tempFilePath, {
+            destination: tempLocalFile
+        }).then(() => {
+            console.log('The file has been downloaded to', tempLocalFile);
+            // Generate a thumbnail using ImageMagick.
+            return exec(`convert "${tempLocalFile}" -thumbnail '${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>' "${tempLocalThumbFile}"`).then(() => {
+                console.log('Thumbnail created at', tempLocalThumbFile);
+                // Uploading the Thumbnail.
+                return bucket.upload(tempLocalThumbFile, {
                     destination: thumbFilePath
-                })
-            })
-
+                }).then(() => {
+                    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+                });
+            });
+        });
     });
-
-
+});
