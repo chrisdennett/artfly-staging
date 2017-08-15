5023adf7b57d0c5d0756adb30a815a271b187133
chrisdennett <chrisdennett@gmail.com>
2017-08-15 11:16:24 +0100

    Move slim cropper code to correct directory.

--- src/components/ImageCropAndRotate/ImageCropAndRotate.js
+++ src/components/ImageCropAndRotate/ImageCropAndRotate.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
-import Slim from '../ArtworkEditor/slim/slim.react';
+import Slim from './slim/slim.react';
 
 // Slim Image Cropper - docs - http://slimimagecropper.com/
 class ImageCropAndRotate extends Component {
 
     slimConfirm(slimData) {
-        if (this.props.onCropDataChange) {
+        if (this.props.onCropDataConfirm) {
             const { actions, input } = slimData;
             const { crop, rotation } = actions; // NB crop contains a type property which is set to manual if
             const { type } = input;
             const { height, width } = crop;
 
-            this.props.onCropDataChange({ height, width, crop, rotation, type });
+            this.props.onCropDataConfirm({ height, width, crop, rotation, type });
         }
     }
 
     didInit(slimData) {
         // triggers the creation of an image file from default canvas size
-        const cropper = this.cropRef.instance;
-        cropper.upload();
+        if (this.props.callSaveOnImageLoad) {
+            const cropper = this.cropRef.instance;
+            cropper.upload();
+        }
 
-        if (this.props.onCropDataChange) {
+        if (this.props.onCropDataInit) {
             const { actions, input } = slimData;
             const { crop, rotation } = actions; // NB crop contains a type property which is set to manual if
             const { type } = input;
             const { height, width } = crop;
 
-            this.props.onCropDataChange({ height, width, crop, rotation, type });
+            this.props.onCropDataInit({ height, width, crop, rotation, type });
         }
     }
 
@@ -36,7 +38,7 @@ class ImageCropAndRotate extends Component {
     slim-upload-status
     */
 
-      // called when upload button is pressed or automatically if push is enabled
+    // called when upload button is pressed or automatically if push is enabled
     slimService(formdata, progress, success, failure, slim) {
         const blob = formdata[0];
         // this.props.onCropImageSave({ image, height, width, crop, rotation, type });
@@ -47,13 +49,14 @@ class ImageCropAndRotate extends Component {
         success(null);
     }
 
-    openEditScreen(){
+    openEditScreen() {
         const cropper = this.cropRef.instance;
         cropper.edit();
     }
 
     render() {
         const showDownload = this.props.showDownload ? this.props.showDownload : false;
+        const callSaveOnImageLoad = this.props.callSaveOnImageLoad ? this.props.callSaveOnImageLoad : false;
 
         return (
             <div>
@@ -62,10 +65,11 @@ class ImageCropAndRotate extends Component {
                       ref={(cropRef) => { this.cropRef = cropRef; }}
                       size="3500,3500"
                       initialImage={this.props.url}
+                      callSaveOnImageLoad={callSaveOnImageLoad}
                       serviceFormat="file"
                       push="true"
                       statusUploadSuccess=""
-                      service={ this.slimService.bind(this) }
+                      service={this.slimService.bind(this)}
                       didInit={this.didInit.bind(this)}
                       didConfirm={this.slimConfirm.bind(this)}>
 
