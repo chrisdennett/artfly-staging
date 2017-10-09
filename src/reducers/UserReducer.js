4abe03daf8a389d871b4a738b0bec8718339819a
chrisdennett <chrisdennett@gmail.com>
2017-10-09 10:50:12 +0100

    UserReducer - got total artworks being calculated in user reducer ever time an artist artwork Ids changes.

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -5,6 +5,8 @@ import {
     DELETE_USER
 } from '../actions/UserActions';
 
+import {ARTIST_ARTWORK_IDS_CHANGE} from '../actions/ArtistGalleryActions';
+
 export default function (state = {}, action) {
 
     switch (action.type) {
@@ -22,6 +24,18 @@ export default function (state = {}, action) {
             // Doesn't need to return anything because there is a libs listener on the user
             // that will trigger FETCH_USER
             return state;
+
+        case ARTIST_ARTWORK_IDS_CHANGE:
+            //NB The ids data value is the date added which is used for sorting
+            const {artistId, totalArtworks} = action.payload;
+            const currentArtistTotals = {...state.artistArtworkTotals, [artistId]:totalArtworks};
+            const artistIds = Object.keys(currentArtistTotals);
+            let total = 0;
+            for(let id of artistIds){
+                total += currentArtistTotals[id];
+            }
+
+            return { ...state, artistArtworkTotals: currentArtistTotals, totalArtworks:total };
 
         default:
             return state;
