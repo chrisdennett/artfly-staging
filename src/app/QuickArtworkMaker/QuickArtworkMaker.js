0bdc1f7b82c99f06c21079031d4674a7ca3d93c6
chrisdennett <chrisdennett@gmail.com>
2018-01-31 13:42:31 +0000

    Got quickArtwork dealing with image uploading and display.

--- src/app/QuickArtworkMaker/QuickArtworkMaker.js
+++ src/app/QuickArtworkMaker/QuickArtworkMaker.js
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
+import sizeMe from 'react-sizeme';
 // styles
 import './quickArtworkMaker_styles.css';
 // comps
 import QuickPhotoSelector from "./quickPhotoSelector/QuickPhotoSelector";
 import QuickArtworkRoom from "./quickArtworkRoom/QuickArtworkRoom";
+import * as ImageHelper from "../ArtStudio/ImageHelper";
+import QuickArtwork from "./quickArtwork/QuickArtwork";
 
 class QuickArtworkMaker extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onPhotoSelect = this.onPhotoSelect.bind(this);
+
+        this.state = { artworkData: null };
+    }
+
+    onPhotoSelect(imgFile) {
+        ImageHelper.GetImage(imgFile, (sourceImg, imgOrientation, widthToHeightRatio, heightToWidthRatio) => {
+            this.setState({ artworkData: { sourceImg, imgOrientation, widthToHeightRatio, heightToWidthRatio } });
+        });
+    }
+
     render() {
+        const { artworkData } = this.state;
+        const { height, width } = this.props.size;
+        const floorPercentage = 0.15;
+        const floorHeight = height * floorPercentage;
+
         return (
             <div className={'quickArtworkMaker'}>
 
-                <QuickArtworkRoom/>
+                <QuickArtworkRoom height={height}
+                                  floorHeight={floorHeight}/>
 
-                <div className={'quickArtworkMaker--photoSelectorHolder'}>
-                    <QuickPhotoSelector/>
+                {artworkData &&
+                <div className={'quickArtworkMaker--artworkHolder'}>
+                    <QuickArtwork height={height-floorHeight}
+                                  width={width}
+                                  artworkData={artworkData}/>
                 </div>
+                }
+
+                {!artworkData &&
+                <div className={'quickArtworkMaker--photoSelectorHolder'}>
+                    <QuickPhotoSelector onPhotoSelected={this.onPhotoSelect}/>
+                </div>
+                }
             </div>
         );
     }
 }
 
-export default QuickArtworkMaker;
+export default sizeMe({ monitorHeight: true })(QuickArtworkMaker);
--- src/app/QuickArtworkMaker/quickArtworkRoom/QuickArtworkRoom.js
+++ src/app/QuickArtworkMaker/quickArtworkRoom/QuickArtworkRoom.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import sizeMe from 'react-sizeme';
-//
+import React from 'react';
+// comps
 import './quickArtworkRoom_styles.css';
-// import Flooring from "../../global/flooring/Flooring";
 import QuickArtworkRoomFloor from "./quickArtworkRoom--floor/QuickArtworkRoomFloor";
 
-// import BrickWall from '../../images/brickwall.png';
+const QuickArtworkRoom = ({ height, floorHeight }) => {
 
-class QuickArtworkRoom extends Component {
+    const vignetteStyle = { height: height - floorHeight };
 
-    render() {
+    return (
+        <div className={'quickArtworkRoom'}>
+            <div className={'quickArtworkRoom--vignette'} style={vignetteStyle}/>
 
-        const { height } = this.props.size;
-        const floorPercentage = 0.15;
-        const floorHeight = height * floorPercentage;
-
-        return (
-            <div className={'quickArtworkRoom'}>
-                <div className={'quickArtworkRoom--floorHolder'}>
-                    <QuickArtworkRoomFloor height={floorHeight}/>
-                </div>
+            <div className={'quickArtworkRoom--floorHolder'}>
+                <QuickArtworkRoomFloor height={floorHeight}/>
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
 
-export default sizeMe({ monitorHeight: true })(QuickArtworkRoom);
+export default QuickArtworkRoom;
