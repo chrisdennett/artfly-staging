705462a22605c03702fcfcc581b1d8b095a79d1b
chrisdennett <chrisdennett@gmail.com>
2018-05-02 16:44:27 +0100

    Tied up people slider to artwork data.

--- src/app/artworkOptions/peopleOptions/PeoplesOptions.js
+++ src/app/artworkOptions/peopleOptions/PeoplesOptions.js
@@ -4,55 +4,51 @@ import { Slider } from 'rmwc/Slider';
 import './peopleOptions_styles.css';
 import HorizontalList from "../../global/horizontalList/HorizontalList";
 
+const baseUrl = '/images/audience/';
 const peopleOptionData = {
-    baseUrl: '/images/audience/',
-    people: {
-        womanStanding1: {
-            presetName: 'womanStanding1',
-            thumb: 'woman-standing-1-darker-thumb.png',
-            fileName: 'woman-standing-1-darker.png',
-            height: 300, width: 120,
-            maxProportionOfScreenHeight: 0.5
-        },
-        peopleSitting1: {
-            thumb: 'people-sitting-1-thumb.png',
-            fileName: 'people-sitting-1.png',
-            height: 200, width: 310,
-            maxProportionOfScreenHeight: 0.3
-        },
-        peopleWalking1: {
-            thumb: 'people-walking-1-thumb.png',
-            fileName: 'people-walking-1.png',
-            height: 320, width: 224,
-            maxProportionOfScreenHeight: 0.5
-        }
+    womanStanding1: {
+        thumb: `${baseUrl}woman-standing-1-darker-thumb.png`,
+        url: `${baseUrl}woman-standing-1-darker.png`,
+        height: 300, width: 120,
+        maxProportionOfScreenHeight: 0.5
+    },
+    peopleSitting1: {
+        thumb: `${baseUrl}people-sitting-1-thumb.png`,
+        url: `${baseUrl}people-sitting-1.png`,
+        height: 200, width: 310,
+        maxProportionOfScreenHeight: 0.3
+    },
+    peopleWalking1: {
+        thumb: `${baseUrl}people-walking-1-thumb.png`,
+        url: `${baseUrl}people-walking-1.png`,
+        height: 320, width: 224,
+        maxProportionOfScreenHeight: 0.5
     }
 };
 
 const PeopleOptions = ({ people, onDataChange }) => {
 
     const peopleIds = Object.keys(people);
-    const peopleOptionsIds = Object.keys(peopleOptionData.people);
-    const baseUrl = peopleOptionData.baseUrl;
-
-    console.log("peopleIds: ", peopleIds);
+    const peopleOptionsIds = Object.keys(peopleOptionData);
+        const firstPersonId = peopleIds[0];
+        const person = people[firstPersonId];
 
     const onPersonPositionSliderChangeX = (x) => {
-        console.log("x: ", x);
 
+        const roundedX = Math.round(x * 1000) / 1000;
 
+        const updatedPersonData = { ...person, x: roundedX };
+        const updatedPeople = {...people, [firstPersonId]:updatedPersonData};
 
-        /*const newFrameData = { ...frameData, mountThicknessDecimal };
-        onDataChange({ frameData: newFrameData });*/
+        onDataChange({ people: updatedPeople });
     };
 
     return (
         <div style={{ padding: 10 }} className={'pretty-scroll'}>
             <HorizontalList>
                 {peopleOptionsIds.map(id => {
-                    const option = peopleOptionData.people[id];
-
-                    return <div key={id}><img src={baseUrl+option.thumb} /></div>
+                    const option = peopleOptionData[id];
+                    return <div key={id}><img src={option.thumb}/></div>
                 })
 
                 }
@@ -61,7 +57,7 @@ const PeopleOptions = ({ people, onDataChange }) => {
             Position:
             <Slider
                 min={0} max={1}
-                value={0.5}
+                value={person.x}
                 onInput={e => onPersonPositionSliderChangeX(e.detail.value)}
             />
         </div>
