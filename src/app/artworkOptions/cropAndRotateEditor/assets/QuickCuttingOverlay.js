75ef5f4d585e7b0089fde1d01348e18f48d6dd23
chrisdennett <chrisdennett@gmail.com>
2018-05-08 13:43:00 +0100

    Added background to the cutting mat.

--- src/app/artworkOptions/cropAndRotateEditor/assets/QuickCuttingOverlay.js
+++ src/app/artworkOptions/cropAndRotateEditor/assets/QuickCuttingOverlay.js
@@ -96,19 +96,19 @@ class QuickCuttingOverlay extends Component {
 
                     <g transform={`translate(${middleX - 25}, ${middleY - 25})`} onClick={onRotateClick}>
 
-                        <circle cx={25} cy={25} r={25} fill={'rgba(255,255,0, 0.7)'}
+                        <circle cx={25} cy={25} r={25} fill={'rgba(2,112,85, 1)'}
                                 stroke={'none'}/>
 
-                        <svg fill="#000000" height="48" viewBox="0 0 24 24" width="48"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M7.34 6.41L.86 12.9l6.49 6.48 6.49-6.48-6.5-6.49zM3.69 12.9l3.66-3.66L11 12.9l-3.66 3.66-3.65-3.66zm15.67-6.26C17.61 4.88 15.3 4 13 4V.76L8.76 5 13 9.24V6c1.79 0 3.58.68 4.95 2.05 2.73 2.73 2.73 7.17 0 9.9C16.58 19.32 14.79 20 13 20c-.97 0-1.94-.21-2.84-.61l-1.49 1.49C10.02 21.62 11.51 22 13 22c2.3 0 4.61-.88 6.36-2.64 3.52-3.51 3.52-9.21 0-12.72z"/>
-                            <path d="M0 0h24v24H0z" fill="none"/>
-                        </svg>
+                        <g transform={`translate(9, 9)`}>
+                            <svg fill="#000000" height="30" width="30" viewBox="0 0 24 24"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill={'#d4c63f'}
+                                      d="M7.34 6.41L.86 12.9l6.49 6.48 6.49-6.48-6.5-6.49zM3.69 12.9l3.66-3.66L11 12.9l-3.66 3.66-3.65-3.66zm15.67-6.26C17.61 4.88 15.3 4 13 4V.76L8.76 5 13 9.24V6c1.79 0 3.58.68 4.95 2.05 2.73 2.73 2.73 7.17 0 9.9C16.58 19.32 14.79 20 13 20c-.97 0-1.94-.21-2.84-.61l-1.49 1.49C10.02 21.62 11.51 22 13 22c2.3 0 4.61-.88 6.36-2.64 3.52-3.51 3.52-9.21 0-12.72z"/>
+                                <path d="M0 0h24v24H0z" stroke='none' fill="none"/>
+                            </svg>
+                        </g>
                     </g>
-
                 </svg>
-
 
                 <DragHandle id={'top-left'}
                             bounds={{ left: 0, right: rightX - 50, top: 0, bottom: bottomY - 50 }}
