ef2b3b37598f1516246d3b5d730698443f5037de
chrisdennett <chrisdennett@gmail.com>
2018-05-16 11:07:44 +0100

    Separated store into it's own file for single purpose clarity.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 // externals
-import "babel-polyfill";
+import "babel-polyfill"; // is this needed still
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension'; // handy for debugging
-// reducer index.js
-import reducers from './actionReducers/index';
+// store - where the entire app state lives
+import store from './store';
 // homebrew routing
 import AppRouting from "./AppRouting";
-
-// redux store using thunk for async actions e.g used with firebase calls
-// const store = createStore(reducers, applyMiddleware(thunk));
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 // Provider allows any child component to connect to the redux store
 const provider = (
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from "../actionReducers"; // handy for debugging
+
+// redux store using thunk for async actions e.g used with firebase calls
+// const store = createStore(reducers, applyMiddleware(thunk));
+// const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
