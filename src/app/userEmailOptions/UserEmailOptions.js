9d821829b62948e59b9f90e3768357d4de463027
chrisdennett <chrisdennett@gmail.com>
2018-05-22 14:23:51 +0100

    User profile page - email section formatting and hooking up to user update.

--- src/app/userEmailOptions/UserEmailOptions.js
+++ src/app/userEmailOptions/UserEmailOptions.js
@@ -1,46 +1,55 @@
 import React, { Component } from "react";
 import { Button, ButtonIcon } from 'rmwc/Button';
-import { Switch } from 'rmwc/Switch';
 import { TextField } from 'rmwc/TextField';
+import { Switch } from 'rmwc/Switch'
 
 class UserEmailOptions extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = { inEditMode: true, unsavedEmail: '' }
     }
 
     render() {
         const { userEmail, allowEmailUpdates, updateUser, userId } = this.props;
-
-        const emailButtonText = userEmail ? 'Change email' : 'Add email';
+        const { inEditMode, unsavedEmail } = this.state;
+        let currentEmailValue = unsavedEmail.length > 0 ? unsavedEmail : userEmail;
+        if (currentEmailValue === null) currentEmailValue = '';
+        const hasUnsavedChanges = unsavedEmail.length > 0 && userEmail !== unsavedEmail;
 
         return (
             <div>
-                <div className={'userProfile--detail'} style={{padding:0}}>
-                    <div className={'userProfile--detail--type'}>
-                        <TextField type={'email'}
-                                   outlined
-                                   className={'userProfile--emailTextfield'}
-                                   label={'Email address:'}/>
-                    </div>
+                <div className={'userProfile--detail--col'} style={{ padding: 0 }}>
+                    {inEditMode &&
+                    <TextField type={'email'}
+                               className={'userProfile--email'}
+                               outlined
+                               onChange={(e) => this.setState({ unsavedEmail: e.target.value })}
+                               value={currentEmailValue}
+                               label='Email address:'/>
+                    }
 
-                    <div className={'userProfile--detail--value'}>
-                        <Button raised><ButtonIcon>email</ButtonIcon>{emailButtonText}</Button>
+                    {hasUnsavedChanges &&
+                    <div className={'userProfile--detail--buttons'}>
+                        <Button>Cancel</Button>
+                        <Button raised
+                                onClick={() => updateUser(userId, {email:currentEmailValue})}
+                                theme="secondary-bg on-secondary">
+                            <ButtonIcon>save</ButtonIcon>
+                            Save
+                        </Button>
                     </div>
+                    }
 
+                    {userEmail &&
+                    <Switch
+                        checked={allowEmailUpdates}
+                        onChange={() => updateUser(userId, {allowEmailUpdates: !allowEmailUpdates})}>
+                        Get email updates
+                    </Switch>
+                    }
                 </div>
-                {userEmail &&
-                <div className={'userProfile--detail'}>
-                    <div className={'userProfile--detail--type'}>Let me send you email updates:</div>
-                    <div className={'userProfile--detail--value'}>
-                        <Switch
-                            checked={allowEmailUpdates}
-                            onChange={e => updateUser(userId, { allowEmailUpdates: e.target.checked })}>
-                            Allow email updates
-                        </Switch>
-                    </div>
-                </div>
-                }
             </div>
         );
     }
