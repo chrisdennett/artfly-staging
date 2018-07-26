cec9d392bc24c6b4c9048de9ab57ca403403b166
chrisdennett <chrisdennett@gmail.com>
2018-07-26 13:53:31 +0100

    Removed user avatar photo because it started going wrong for google because of cross domain issues.  It's not needed so I ditched it.

--- src/app/userProfile/AuthDetails.js
+++ src/app/userProfile/AuthDetails.js
@@ -1,29 +1,9 @@
 import React from 'react';
 
 const AuthDetails = function ({ user, userSignInMethod }) {
-
-    // get the smaller sized image if it's google
-    let photoUrl;
-
-    if(userSignInMethod === 'GOOGLE') {
-        photoUrl = user.photoURL + '?sz=80';
-    }
-    else if(userSignInMethod === 'EMAIL / PASSWORD'){
-        photoUrl = null;
-    }
-    else {
-        photoUrl = user.photoURL;
-    }
-
     return (
         <div>
             <div className={'userProfile--detailsList'}>
-                {photoUrl &&
-                <div className={'userProfile--detailsList--avatarHolder'}>
-                    <img src={photoUrl} alt={'user avatar'}/>
-                </div>
-                }
-
                 <div className={'userProfile--detail'}>
                     <div className={'userProfile--detail--type'}>Name:</div>
                     <div className={'userProfile--detail--value'}>{user.displayName}</div>
