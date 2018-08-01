50877ee08943355951387dcc1e5fb30974f8fa6c
chrisdennett <chrisdennett@gmail.com>
2018-08-01 16:23:37 +0100

    Added links to the terms of service and the privacy policy on the home page footer.

--- src/app/home/JumpingVictorianLady.js
+++ src/app/home/JumpingVictorianLady.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+// ui
 import { Typography } from 'rmwc/Typography';
+import { Button } from 'rmwc/Button';
+// actions
+import { UpdateUrl } from "../../actions/UrlActions";
 // comps
 import SocialMediaLinks from './socialMediaStuff/SocialMediaLinks';
 
-const JumpingVictorianLady = () => {
+const JumpingVictorianLady = ({UpdateUrl}) => {
     return (
         <div style={{
-            backgroundColor: '#000',
+            backgroundColor: '#1b1b1b',
             color: '#fff',
-            border: '10px solid #555', padding: 42, textAlign: 'center'
+            padding: 42, textAlign: 'center'
         }}>
             <img style={{ background: '#fff', padding: 10 }} src={'images/lab/Rotoscoping.gif'}
                  alt={'rotoscoping gif'}/>
@@ -17,9 +21,13 @@ const JumpingVictorianLady = () => {
                 <p>You know it's all over when the victorian lady jumps the stool...</p>
             </Typography>
 
-            <SocialMediaLinks />
+            <SocialMediaLinks/>
+
+            <Button style={{color:'#fff'}} onClick={() => UpdateUrl('/termsOfService')}>Terms of Service</Button>
+            |
+            <Button style={{color:'#fff'}} onClick={() => UpdateUrl('/privacyPolicy')}>Privacy Policy</Button>
         </div>
     )
 };
 
-export default JumpingVictorianLady;
+export default connect(null, { UpdateUrl })(JumpingVictorianLady);
